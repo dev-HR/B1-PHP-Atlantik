5d64aa1a7729a9042c4b6ff038aafb2db27dbbb7
dev-HR <66364380+dev-HR@users.noreply.github.com>
2021-12-01 13:13:06 +0100

    Add files via upload

---
+++ calculerEffectifsParFonction.php
@@ -0,0 +1,30 @@
+<?php
+
+	$persoNavig = array(
+		array( "matricule" => 1001 , "nom" => "GENPRI"  , "prénom" => "Erwan" , "âge" => 54 , "année" => 2010 , "fonction" => "Barman" ) ,
+		array( "matricule" => 1002 , "nom" => "KERREH"  , "prénom" => "Armelle" , "âge" => 55 , "année" => 2010 , "fonction" => "Capitaine" ) ,
+		array( "matricule" => 1010 , "nom" => "MATURIN"  , "prénom" => "Yvan" , "âge" => 42 , "année" => 2011 , "fonction" => "Steward" ) ,
+		array( "matricule" => 1011 , "nom" => "OUERTANI"  , "prénom" => "Ines" , "âge" => 45 , "année" => 2011 , "fonction" => "Steward" ) ,
+		array( "matricule" => 1012 , "nom" => "PICOIS"  , "prénom" => "Charles" , "âge" => 38 , "année" => 2012 , "fonction" => "Steward" ) ,
+		array( "matricule" => 1015 , "nom" => "BELAROUSSI"  , "prénom" => "Idriss" , "âge" => 32 , "année" => 2012 , "fonction" => "Barman" ) ,
+		array( "matricule" => 1017 , "nom" => "BENAIOUN"  , "prénom" => "Paul" , "âge" => 47 , "année" => 2012 , "fonction" => "Steward" ) ,
+		array( "matricule" => 1020 , "nom" => "HELDERAL"  , "prénom" => "Mickaël" , "âge" => 38 , "année" => 2012 , "fonction" => "Steward" ) ,
+		array( "matricule" => 1057 , "nom" => "RAVIER"  , "prénom" => "Amandine" , "âge" => 30 , "année" => 2013 , "fonction" => "Steward" ) ,
+		array( "matricule" => 1088 , "nom" => "ROUGER"  , "prénom" => "Frédéric" , "âge" => 33 , "année" => 2013 , "fonction" => "Steward" ) ,
+		array( "matricule" => 1090 , "nom" => "NAUD"  , "prénom" => "Mickaël" , "âge" => 28 , "année" => 2014 , "fonction" => "Barman" ) ,
+		array( "matricule" => 1101 , "nom" => "TURPIN"  , "prénom" => "Mike" , "âge" => 55 , "année" => 2014 , "fonction" => "Capitaine" ) ,
+		array( "matricule" => 1137 , "nom" => "WALLACE"  , "prénom" => "Jean" , "âge" => 61 , "année" => 2015 , "fonction" => "Steward" ) ,
+		array( "matricule" => 1142 , "nom" => "LOPES"  , "prénom" => "José" , "âge" => 29 , "année" => 2015 , "fonction" => "Capitaine" ) ,
+		array( "matricule" => 1155 , "nom" => "AMINDADA"  , "prénom" => "Mamadou" , "âge" => 48 , "année" => 2015 , "fonction" => "Steward" ) ,
+		array( "matricule" => 1167 , "nom" => "EIBERT"  , "prénom" => "Julien" , "âge" => 49 , "année" => 2020 , "fonction" => "Barman" ) ,
+		array( "matricule" => 1168 , "nom" => "LI"  , "prénom" => "Anaïs" , "âge" =>  38 , "année" => 2020 , "fonction" => "Steward" ) ,
+		array( "matricule" => 1171 , "nom" => "BA"  , "prénom" => "Béchir" , "âge" => 22 , "année" => 2020 , "fonction" => "Steward" )
+	) ;
+
+	$fonctionsEffectifs = array() ;
+
+	// Votre code ici
+	
+	
+
+?>
